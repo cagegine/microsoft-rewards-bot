e64c9065e08a4207421cee7c265cc963a39053c8
cagegine <cagegine@gmail.com>
2023-11-02 12:09:14 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ git pull > /dev/null
 # install any new dependencies
 pip install --root-user-action=ignore -r requirements.txt > /dev/null
 
+# remove temp file when previous execution crashed
+rm /tmp/.X99-lock
+
 # set display port and dbus env to avoid hanging (https://github.com/joyzoursky/docker-python-chromedriver)
 export DISPLAY=:99
 export DBUS_SESSION_BUS_ADDRESS=/dev/null
